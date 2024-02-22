b14fb80aa21cb3312910a45cef3132c513752dcd
jan.nijtmans <jan.nijtmans@noemail.net>
2024-02-22 17:40:55 +0000

    Fix [006bd0c74e]: PATCH: BIO_CTRL_PUSH not handled. Also add some changes from androwish: [https://www.androwish.org/home/info/982ebf9d31a60440]
    FossilOrigin-Name: 1b8c76f78306a5bf9ad2fad11e32c82e239deeffb5e7490d608d1ba55217b14d

--- generic/tlsBIO.c
+++ generic/tlsBIO.c
@@ -27,6 +27,7 @@
 #define BIO_meth_set_destroy(bio, val)   (bio)->destroy = val;
 #endif
 
+#if 0
 /*
  * Forward declarations
  */
@@ -37,6 +38,254 @@ static int BioPuts  (BIO *h, const char *str);
 static long BioCtrl (BIO *h, int cmd, long arg1, void *ptr);
 static int BioNew   (BIO *h);
 static int BioFree  (BIO *h);
+#endif
+
+static int BioWrite(BIO *bio, const char *buf, int bufLen) {
+    Tcl_Channel chan;
+    Tcl_Size ret;
+    int tclEofChan, tclErrno;
+
+    chan = Tls_GetParent((State *) BIO_get_data(bio), 0);
+
+    dprintf("[chan=%p] BioWrite(%p, <buf>, %d)", (void *)chan, (void *) bio, bufLen);
+
+    ret = Tcl_WriteRaw(chan, buf, (Tcl_Size)bufLen);
+
+    tclEofChan = Tcl_Eof(chan);
+    tclErrno = Tcl_GetErrno();
+
+    dprintf("[chan=%p] BioWrite(%d) -> %" TCL_SIZE_MODIFIER "d [tclEof=%d; tclErrno=%d]",
+	    chan, bufLen, ret, tclEofChan, tclErrno);
+
+    BIO_clear_flags(bio, BIO_FLAGS_WRITE | BIO_FLAGS_SHOULD_RETRY);
+
+    if (tclEofChan && ret <= 0) {
+	dprintf("Got EOF while reading, returning a Connection Reset error which maps to Soft EOF");
+	Tcl_SetErrno(ECONNRESET);
+	ret = 0;
+    } else if (ret == 0) {
+	dprintf("Got 0 from Tcl_WriteRaw, and EOF is not set; ret = 0");
+	dprintf("Setting retry read flag");
+	BIO_set_retry_read(bio);
+    } else if (ret < 0) {
+	dprintf("We got some kind of I/O error");
+
+	if (tclErrno == EAGAIN) {
+	    dprintf("It's EAGAIN");
+	} else {
+	    dprintf("It's an unexpected error: %s/%i", Tcl_ErrnoMsg(tclErrno), tclErrno);
+	}
+    } else {
+	dprintf("Successfully wrote %" TCL_SIZE_MODIFIER "d bytes of data", ret);
+    }
+
+    if (ret != -1 || (ret == -1 && tclErrno == EAGAIN)) {
+	if (BIO_should_read(bio)) {
+	    dprintf("Setting should retry read flag");
+
+	    BIO_set_retry_read(bio);
+	}
+    }
+    return (int)ret;
+}
+
+/* Called by SSL_read()*/
+static int BioRead(BIO *bio, char *buf, int bufLen) {
+    Tcl_Channel chan;
+    Tcl_Size ret = 0;
+    int tclEofChan, tclErrno;
+
+    chan = Tls_GetParent((State *) BIO_get_data(bio), 0);
+
+    dprintf("[chan=%p] BioRead(%p, <buf>, %d)", (void *) chan, (void *) bio, bufLen);
+
+    if (buf == NULL) {
+	return 0;
+    }
+
+    ret = Tcl_ReadRaw(chan, buf, (Tcl_Size)bufLen);
+
+    tclEofChan = Tcl_Eof(chan);
+    tclErrno = Tcl_GetErrno();
+
+    dprintf("[chan=%p] BioRead(%d) -> %" TCL_SIZE_MODIFIER "d [tclEof=%d; tclErrno=%d]",
+	    chan, bufLen, ret, tclEofChan, tclErrno);
+
+    BIO_clear_flags(bio, BIO_FLAGS_READ | BIO_FLAGS_SHOULD_RETRY);
+
+    if (tclEofChan && ret <= 0) {
+	dprintf("Got EOF while reading, returning a Connection Reset error which maps to Soft EOF");
+	Tcl_SetErrno(ECONNRESET);
+	ret = 0;
+    } else if (ret == 0) {
+	dprintf("Got 0 from Tcl_Read or Tcl_ReadRaw, and EOF is not set; ret = 0");
+	dprintf("Setting retry read flag");
+	BIO_set_retry_read(bio);
+    } else if (ret < 0) {
+	dprintf("We got some kind of I/O error");
+
+	if (tclErrno == EAGAIN) {
+	    dprintf("It's EAGAIN");
+	} else {
+	    dprintf("It's an unexpected error: %s/%i", Tcl_ErrnoMsg(tclErrno), tclErrno);
+	}
+    } else {
+	dprintf("Successfully read %" TCL_SIZE_MODIFIER "d bytes of data", ret);
+    }
+
+    if (ret != -1 || (ret == -1 && tclErrno == EAGAIN)) {
+	if (BIO_should_write(bio)) {
+	    dprintf("Setting should retry write flag");
+
+	    BIO_set_retry_write(bio);
+	}
+    }
+
+    dprintf("BioRead(%p, <buf>, %d) [%p] returning %" TCL_SIZE_MODIFIER "d",
+	    bio, bufLen, (void *) chan, ret);
+
+    return (int)ret;
+}
+
+static int BioPuts(BIO *bio, const char *str) {
+    dprintf("BioPuts(%p, <string:%p>) called", bio, str);
+
+    return BioWrite(bio, str, (int) strlen(str));
+}
+
+static long BioCtrl(BIO *bio, int cmd, long num, void *ptr) {
+    Tcl_Channel chan;
+    long ret = 1;
+
+    chan = Tls_GetParent((State *) BIO_get_data(bio), 0);
+
+    dprintf("BioCtrl(%p, 0x%x, 0x%lx, %p)", bio, cmd, num, ptr);
+
+    switch (cmd) {
+	case BIO_CTRL_RESET:
+		dprintf("Got BIO_CTRL_RESET");
+		ret = 0;
+		break;
+	case BIO_C_FILE_SEEK:
+		dprintf("Got BIO_C_FILE_SEEK");
+		ret = 0;
+		break;
+	case BIO_C_FILE_TELL:
+		dprintf("Got BIO_C_FILE_TELL");
+		ret = 0;
+		break;
+	case BIO_CTRL_INFO:
+		dprintf("Got BIO_CTRL_INFO");
+		ret = 1;
+		break;
+	case BIO_C_SET_FD:
+		dprintf("Unsupported call: BIO_C_SET_FD");
+		ret = -1;
+		break;
+	case BIO_C_GET_FD:
+		dprintf("Unsupported call: BIO_C_GET_FD");
+		ret = -1;
+		break;
+	case BIO_CTRL_GET_CLOSE:
+		dprintf("Got BIO_CTRL_CLOSE");
+		ret = BIO_get_shutdown(bio);
+		break;
+	case BIO_CTRL_SET_CLOSE:
+		dprintf("Got BIO_SET_CLOSE");
+		BIO_set_shutdown(bio, num);
+		break;
+	case BIO_CTRL_EOF:
+		dprintf("Got BIO_CTRL_EOF");
+		ret = ((chan) ? Tcl_Eof(chan) : 1);
+		break;
+	case BIO_CTRL_PENDING:
+		dprintf("Got BIO_CTRL_PENDING");
+		ret = ((chan) ? ((Tcl_InputBuffered(chan) ? 1 : 0)) : 0);
+		dprintf("BIO_CTRL_PENDING(%d)", (int) ret);
+		break;
+	case BIO_CTRL_WPENDING:
+		dprintf("Got BIO_CTRL_WPENDING");
+		ret = 0;
+		break;
+	case BIO_CTRL_DUP:
+		dprintf("Got BIO_CTRL_DUP");
+		break;
+	case BIO_CTRL_FLUSH:
+		dprintf("Got BIO_CTRL_FLUSH");
+		ret = ((chan) && (Tcl_WriteRaw(chan, "", 0) >= 0) ? 1 : -1);
+		dprintf("BIO_CTRL_FLUSH returning value %li", ret);
+		break;
+#ifdef BIO_CTRL_PUSH
+	case BIO_CTRL_PUSH:
+		dprintf("Got BIO_CTRL_PUSH");
+		ret = 0;
+		break;
+#endif
+#ifdef BIO_CTRL_POP
+	case BIO_CTRL_POP:
+		dprintf("Got BIO_CTRL_POP");
+		ret = 0;
+		break;
+#endif
+#ifdef BIO_CTRL_SET
+	case BIO_CTRL_SET:
+		dprintf("Got BIO_CTRL_SET");
+		ret = 0;
+		break;
+#endif
+#ifdef BIO_CTRL_GET
+	case BIO_CTRL_GET :
+		dprintf("Got BIO_CTRL_GET ");
+		ret = 0;
+		break;
+#endif
+#ifdef BIO_CTRL_GET_KTLS_SEND
+	case BIO_CTRL_GET_KTLS_SEND:
+		dprintf("Got BIO_CTRL_GET_KTLS_SEND");
+		ret = 0;
+		break;
+#endif
+#ifdef BIO_CTRL_GET_KTLS_RECV
+	case BIO_CTRL_GET_KTLS_RECV:
+		dprintf("Got BIO_CTRL_GET_KTLS_RECV");
+		ret = 0;
+		break;
+#endif
+	default:
+		dprintf("Got unknown control command (%i)", cmd);
+		ret = -2;
+		break;
+    }
+    return ret;
+}
+
+static int BioNew(BIO *bio) {
+    dprintf("BioNew(%p) called", bio);
+
+    BIO_set_init(bio, 0);
+    BIO_set_data(bio, NULL);
+    BIO_clear_flags(bio, -1);
+    return 1;
+}
+
+static int BioFree(BIO *bio) {
+    if (bio == NULL) {
+	return(0);
+    }
+
+    dprintf("BioFree(%p) called", bio);
+
+    if (BIO_get_shutdown(bio)) {
+	if (BIO_get_init(bio)) {
+	    /*shutdown(bio->num, 2) */
+	    /*closesocket(bio->num) */
+	}
+
+	BIO_set_init(bio, 0);
+	BIO_clear_flags(bio, -1);
+    }
+    return 1;
+}
 
 BIO *BIO_new_tcl(
     State *statePtr,
@@ -110,227 +359,4 @@ BIO *BIO_new_tcl(
     BIO_set_shutdown(bio, flags);
     BIO_set_init(bio, 1);
     return(bio);
-}
-
-static int BioWrite(BIO *bio, const char *buf, int bufLen) {
-    Tcl_Channel chan;
-    int ret;
-    int tclEofChan, tclErrno;
-
-    chan = Tls_GetParent((State *) BIO_get_data(bio), 0);
-
-    dprintf("[chan=%p] BioWrite(%p, <buf>, %d)", (void *)chan, (void *) bio, bufLen);
-
-    ret = Tcl_WriteRaw(chan, buf, bufLen);
-
-    tclEofChan = Tcl_Eof(chan);
-    tclErrno = Tcl_GetErrno();
-
-    dprintf("[chan=%p] BioWrite(%d) -> %d [tclEof=%d; tclErrno=%d]", (void *) chan, bufLen, ret, tclEofChan, Tcl_GetErrno());
-
-    BIO_clear_flags(bio, BIO_FLAGS_WRITE | BIO_FLAGS_SHOULD_RETRY);
-
-    if (tclEofChan && ret <= 0) {
-	dprintf("Got EOF while reading, returning a Connection Reset error which maps to Soft EOF");
-	Tcl_SetErrno(ECONNRESET);
-	ret = 0;
-    } else if (ret == 0) {
-	dprintf("Got 0 from Tcl_WriteRaw, and EOF is not set; ret = 0");
-	dprintf("Setting retry read flag");
-	BIO_set_retry_read(bio);
-    } else if (ret < 0) {
-	dprintf("We got some kind of I/O error");
-
-	if (tclErrno == EAGAIN) {
-	    dprintf("It's EAGAIN");
-	} else {
-	    dprintf("It's an unexpected error: %s/%i", Tcl_ErrnoMsg(tclErrno), tclErrno);
-	}
-    } else {
-	dprintf("Successfully wrote some data");
-    }
-
-    if (ret != -1 || (ret == -1 && tclErrno == EAGAIN)) {
-	if (BIO_should_read(bio)) {
-	    dprintf("Setting should retry read flag");
-
-	    BIO_set_retry_read(bio);
-	}
-    }
-    return(ret);
-}
-
-/* Called by SSL_read()*/
-static int BioRead(BIO *bio, char *buf, int bufLen) {
-    Tcl_Channel chan;
-    int ret = 0;
-    int tclEofChan, tclErrno;
-
-    chan = Tls_GetParent((State *) BIO_get_data(bio), 0);
-
-    dprintf("[chan=%p] BioRead(%p, <buf>, %d)", (void *) chan, (void *) bio, bufLen);
-
-    if (buf == NULL) {
-	return 0;
-    }
-
-    ret = Tcl_ReadRaw(chan, buf, bufLen);
-
-    tclEofChan = Tcl_Eof(chan);
-    tclErrno = Tcl_GetErrno();
-
-    dprintf("[chan=%p] BioRead(%d) -> %d [tclEof=%d; tclErrno=%d]", (void *) chan, bufLen, ret, tclEofChan, tclErrno);
-
-    BIO_clear_flags(bio, BIO_FLAGS_READ | BIO_FLAGS_SHOULD_RETRY);
-
-    if (tclEofChan && ret <= 0) {
-	dprintf("Got EOF while reading, returning a Connection Reset error which maps to Soft EOF");
-	Tcl_SetErrno(ECONNRESET);
-	ret = 0;
-    } else if (ret == 0) {
-	dprintf("Got 0 from Tcl_Read or Tcl_ReadRaw, and EOF is not set; ret = 0");
-	dprintf("Setting retry read flag");
-	BIO_set_retry_read(bio);
-    } else if (ret < 0) {
-	dprintf("We got some kind of I/O error");
-
-	if (tclErrno == EAGAIN) {
-	    dprintf("It's EAGAIN");
-	} else {
-	    dprintf("It's an unexpected error: %s/%i", Tcl_ErrnoMsg(tclErrno), tclErrno);
-	}
-    } else {
-	dprintf("Successfully read some data");
-    }
-
-    if (ret != -1 || (ret == -1 && tclErrno == EAGAIN)) {
-	if (BIO_should_write(bio)) {
-	    dprintf("Setting should retry write flag");
-
-	    BIO_set_retry_write(bio);
-	}
-    }
-
-    dprintf("BioRead(%p, <buf>, %d) [%p] returning %i", (void *) bio, bufLen, (void *) chan, ret);
-
-    return(ret);
-}
-
-static int BioPuts(BIO *bio, const char *str) {
-    dprintf("BioPuts(%p, <string:%p>) called", bio, str);
-
-    return BioWrite(bio, str, (int) strlen(str));
-}
-
-static long BioCtrl(BIO *bio, int cmd, long num, void *ptr) {
-    Tcl_Channel chan;
-    long ret = 1;
-
-    chan = Tls_GetParent((State *) BIO_get_data(bio), 0);
-
-    dprintf("BioCtrl(%p, 0x%x, 0x%lx, %p)", bio, cmd, num, ptr);
-
-    switch (cmd) {
-	case BIO_CTRL_RESET:
-		dprintf("Got BIO_CTRL_RESET");
-		ret = 0;
-		break;
-	case BIO_C_FILE_SEEK:
-		dprintf("Got BIO_C_FILE_SEEK");
-		ret = 0;
-		break;
-	case BIO_C_FILE_TELL:
-		dprintf("Got BIO_C_FILE_TELL");
-		ret = 0;
-		break;
-	case BIO_CTRL_INFO:
-		dprintf("Got BIO_CTRL_INFO");
-		ret = 1;
-		break;
-	case BIO_C_SET_FD:
-		dprintf("Unsupported call: BIO_C_SET_FD");
-		ret = -1;
-		break;
-	case BIO_C_GET_FD:
-		dprintf("Unsupported call: BIO_C_GET_FD");
-		ret = -1;
-		break;
-	case BIO_CTRL_GET_CLOSE:
-		dprintf("Got BIO_CTRL_CLOSE");
-		ret = BIO_get_shutdown(bio);
-		break;
-	case BIO_CTRL_SET_CLOSE:
-		dprintf("Got BIO_SET_CLOSE");
-		BIO_set_shutdown(bio, num);
-		break;
-	case BIO_CTRL_EOF:
-		dprintf("Got BIO_CTRL_EOF");
-		ret = Tcl_Eof(chan);
-		break;
-	case BIO_CTRL_PENDING:
-		dprintf("Got BIO_CTRL_PENDING");
-		ret = ((chan) ? Tcl_InputBuffered(chan) : 0);
-		dprintf("BIO_CTRL_PENDING(%d)", (int) ret);
-		break;
-	case BIO_CTRL_WPENDING:
-		dprintf("Got BIO_CTRL_WPENDING");
-		ret = 0;
-		break;
-	case BIO_CTRL_DUP:
-		dprintf("Got BIO_CTRL_DUP");
-		break;
-	case BIO_CTRL_FLUSH:
-		dprintf("Got BIO_CTRL_FLUSH");
-		ret = ((Tcl_WriteRaw(chan, "", 0) >= 0) ? 1 : -1);
-		dprintf("BIO_CTRL_FLUSH returning value %li", ret);
-		break;
-#ifdef BIO_CTRL_GET_KTLS_SEND
-	case BIO_CTRL_GET_KTLS_SEND:
-		dprintf("Got BIO_CTRL_GET_KTLS_SEND");
-		ret = 0;
-		break;
-#endif
-#ifdef BIO_CTRL_GET_KTLS_RECV
-	case BIO_CTRL_GET_KTLS_RECV:
-		dprintf("Got BIO_CTRL_GET_KTLS_RECV");
-		ret = 0;
-		break;
-#endif
-	default:
-		dprintf("Got unknown control command (%i)", cmd);
-		ret = -2;
-		break;
-    }
-
-    return(ret);
-}
-
-static int BioNew(BIO *bio) {
-    dprintf("BioNew(%p) called", bio);
-
-    BIO_set_init(bio, 0);
-    BIO_set_data(bio, NULL);
-    BIO_clear_flags(bio, -1);
-
-    return(1);
-}
-
-static int BioFree(BIO *bio) {
-    if (bio == NULL) {
-	return(0);
-    }
-
-    dprintf("BioFree(%p) called", bio);
-
-    if (BIO_get_shutdown(bio)) {
-	if (BIO_get_init(bio)) {
-	    /*shutdown(bio->num, 2) */
-	    /*closesocket(bio->num) */
-	}
-
-	BIO_set_init(bio, 0);
-	BIO_clear_flags(bio, -1);
-    }
-
-    return(1);
 }
