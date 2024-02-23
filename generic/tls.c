53c8505375282547ec562bf6c9e76b66190a917b
jan.nijtmans <jan.nijtmans@noemail.net>
2024-02-23 13:08:37 +0000

    Add "tls::build-info" command
    FossilOrigin-Name: 355a10cf0e0c0395737750aeaecfd5212cba8c130be60cafa56c44019135bbf7

--- generic/tls.c
+++ generic/tls.c
@@ -24,6 +24,7 @@
 
 #include "tlsInt.h"
 #include "tclOpts.h"
+#include "tlsUuid.h"
 #include <stdlib.h>
 
 /* Min OpenSSL version */
@@ -1739,13 +1740,20 @@ void Tls_Clean(State *statePtr) {
  *-------------------------------------------------------------------
  */
 
+#ifndef STRINGIFY
+#  define STRINGIFY(x) STRINGIFY1(x)
+#  define STRINGIFY1(x) #x
+#endif
+
+static const char tlsTclInitScript[] = {
+#include "tls.tcl.h"
+    0x00
+};
+
 DLLEXPORT int Tls_Init(
     Tcl_Interp *interp)
 {
-    const char tlsTclInitScript[] = {
-#include "tls.tcl.h"
-	    0x00
-    };
+    Tcl_CmdInfo info;
 
     dprintf("Called");
 
@@ -1770,10 +1778,69 @@ DLLEXPORT int Tls_Init(
     Tcl_CreateObjCommand(interp, "tls::misc", MiscObjCmd, NULL, 0);
 
     if (interp) {
-	Tcl_Eval(interp, tlsTclInitScript);
+	if (Tcl_Eval(interp, tlsTclInitScript) != TCL_OK) {
+	    return TCL_ERROR;
+	}
     }
 
-    return(Tcl_PkgProvide(interp, "tls", PACKAGE_VERSION));
+    if (Tcl_GetCommandInfo(interp, "::tcl::build-info", &info)) {
+	Tcl_CreateObjCommand(interp, "::tls::build-info",
+		info.objProc, (void *)(
+		    PACKAGE_VERSION "+" STRINGIFY(TLS_VERSION_UUID)
+#if defined(__clang__) && defined(__clang_major__)
+			    ".clang-" STRINGIFY(__clang_major__)
+#if __clang_minor__ < 10
+			    "0"
+#endif
+			    STRINGIFY(__clang_minor__)
+#endif
+#if defined(__cplusplus) && !defined(__OBJC__)
+			    ".cplusplus"
+#endif
+#ifndef NDEBUG
+			    ".debug"
+#endif
+#if !defined(__clang__) && !defined(__INTEL_COMPILER) && defined(__GNUC__)
+			    ".gcc-" STRINGIFY(__GNUC__)
+#if __GNUC_MINOR__ < 10
+			    "0"
+#endif
+			    STRINGIFY(__GNUC_MINOR__)
+#endif
+#ifdef __INTEL_COMPILER
+			    ".icc-" STRINGIFY(__INTEL_COMPILER)
+#endif
+#ifdef TCL_MEM_DEBUG
+			    ".memdebug"
+#endif
+#if defined(_MSC_VER)
+			    ".msvc-" STRINGIFY(_MSC_VER)
+#endif
+#ifdef USE_NMAKE
+			    ".nmake"
+#endif
+#ifndef TCL_CFG_OPTIMIZED
+			    ".no-optimize"
+#endif
+#ifdef __OBJC__
+			    ".objective-c"
+#if defined(__cplusplus)
+			    "plusplus"
+#endif
+#endif
+#ifdef TCL_CFG_PROFILED
+			    ".profile"
+#endif
+#ifdef PURIFY
+			    ".purify"
+#endif
+#ifdef STATIC_BUILD
+			    ".static"
+#endif
+		), NULL);
+    }
+
+    return Tcl_PkgProvideEx(interp, "tls", PACKAGE_VERSION, NULL);
 }
 
 /*
